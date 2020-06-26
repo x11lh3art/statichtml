3569d20f03df15705efad4ca908f47cb2fe53f05
x11lh3art <59479059+x11lh3art@users.noreply.github.com>
2020-06-26 07:00:28 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 from flask import Flask, render_template
 
+#Coding
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
